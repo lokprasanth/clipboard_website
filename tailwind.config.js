3b3651d570cfd2af726e0f4e1d6b120605d0a1e1
lokprasanth <lokprasanth09s@gmail.com>
2023-10-18 21:07:45 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  
+  content: ["./index.html"],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors:{
+        strongcyan:'hsl(171,66%,44%)',
+        lightBlue:'hsl(223,100%,69%)',
+        darkGrayishBlue:'hsl(210,10%,33%)',
+        grayishBlue:'hsl(201,11%,66%)',
+      },
+      fontFamily:{
+        sans:['Bai Jamjuree','sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
